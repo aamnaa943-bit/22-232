e2683568c9291147ea3ab13da45d4c21214fb58b
aamnaa943-bit <aamnaa943@gmail.com>
2025-12-31 18:17:40 +0400

    Add check for empty sales data before analysis
    Fix empty sales list issue

--- sales_analysis.py
+++ sales_analysis.py
@@ -3,8 +3,11 @@ sales = [30, 60, 45]
 
 threshold = 50
 
-for i in range(len(products)):
-    if sales[i] >= threshold:
-        print(products[i], "is Best-Selling")
-    else:
-        print(products[i], "is Not Best-Selling")
+if len(sales) == 0:
+    print("No sales data available")
+else:
+    for i in range(len(products)):
+        if sales[i] >= threshold:
+            print(products[i], "is Best-Selling")
+        else:
+            print(products[i], "is Not Best-Selling")
